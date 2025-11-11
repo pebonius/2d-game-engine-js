32a494d472e3b517a86da0d6aa1aed3969e09455
pebonius <pebonius@gmail.com>
2025-11-11 07:00:52 +0100

    checking if rectangle is in viewport

--- rectangle.js
+++ rectangle.js
@@ -9,6 +9,22 @@ export default class Rectangle {
     this.rotationDeg = rotationDeg;
     this.color = color;
   }
+  isInViewPort(canvas) {
+    return (
+      this.x > 1 - this.width &&
+      this.x + 1 < canvas.width &&
+      this.y > 1 - this.height &&
+      this.y + 1 < canvas.height
+    );
+  }
+  isFullyInViewport(canvas) {
+    return (
+      this.x > 0 &&
+      this.x + this.width < canvas.width &&
+      this.y > 0 &&
+      this.y + this.height < canvas.height
+    );
+  }
   draw(context) {
     drawRectangle(
       context,
--- testingScene.js
+++ testingScene.js
@@ -3,15 +3,21 @@ import Rectangle from "./rectangle.js";
 export default class TestingScene {
   constructor() {
     this.rectangles = [
-      new Rectangle(10, 10, 10, 10, 45, "black"),
+      new Rectangle(20, 10, 20, 20, 0, "black"),
       new Rectangle(50, 50, 30, 20, 0, "brown"),
       new Rectangle(150, 150, 50, 50, 120, "blue"),
     ];
   }
-  update() {
-    this.rectangles.forEach((element) => {
-      element.rotationDeg++;
-    });
+  update(game) {
+    this.rectangles[2].rotationDeg++;
+
+    if (this.rectangles[1].isFullyInViewport(game.canvas)) {
+      this.rectangles[1].x++;
+    }
+
+    if (this.rectangles[0].isInViewPort(game.canvas)) {
+      this.rectangles[0].x--;
+    }
   }
   draw(context) {
     this.rectangles.forEach((element) => {
