e7ac21b45874a249ad3087a806f3062ce9f72dab
pebonius <pebonius@gmail.com>
2025-11-15 23:16:00 +0100

    test normalizeRotationDegrees

--- source/graphics.test.js
+++ source/graphics.test.js
@@ -1,7 +1,16 @@
 import { normalizeRotationDegrees } from "./graphics.js";
-import { doesNotThrow, throws } from "./unitTesting.js";
+import { doesNotThrow, throws, is } from "./unitTesting.js";
 
-function normalizeRotationDegreesTest() {
+function normalizeRotationDegreesValidValuesTest() {
+  const validValues = [-120, 0, 1, 90, 3232879243];
+  validValues.forEach((value) => {
+    doesNotThrow(() => {
+      normalizeRotationDegrees(value);
+    });
+  });
+}
+
+function normalizeRotationDegreesInvalidValuesTest() {
   const invalidValues = ["0", "something", undefined, null, { x: 0 }, []];
   invalidValues.forEach((value) => {
     throws(() => {
@@ -10,4 +19,17 @@ function normalizeRotationDegreesTest() {
   });
 }
 
-export const graphicsTests = [normalizeRotationDegreesTest];
+function normalizeRotationDegreesNormalizesDegrees() {
+  is(normalizeRotationDegrees(360), 0);
+  is(normalizeRotationDegrees(365), 5);
+  is(normalizeRotationDegrees(3605), 5);
+  is(normalizeRotationDegrees(-360), 0);
+  is(normalizeRotationDegrees(-365), 355);
+  is(normalizeRotationDegrees(-3605), 355);
+}
+
+export const graphicsTests = [
+  normalizeRotationDegreesValidValuesTest,
+  normalizeRotationDegreesInvalidValuesTest,
+  normalizeRotationDegreesNormalizesDegrees,
+];
