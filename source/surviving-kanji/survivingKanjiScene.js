8c2ed6d51125af2a7e4aef7e733542479542e523
pebonius <pebonius@gmail.com>
2025-11-18 21:23:25 +0100

    start work on surviving kanji scene

--- source/surviving-kanji/survivingKanjiScene.js
+++ source/surviving-kanji/survivingKanjiScene.js
@@ -1,7 +1,11 @@
+import { drawText } from "../graphics.js";
+
 export default class SurvivingKanjiScene {
   constructor(game) {
     this.game = game;
   }
   update(game) {}
-  draw(context) {}
+  draw(context) {
+    drawText(context, "surviving kanji", 20, "white", 30, 30);
+  }
 }
